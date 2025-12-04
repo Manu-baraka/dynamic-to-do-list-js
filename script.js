67a3588aeefafe14828c0abe08839fb901e17ab5
Emmanuel <emmanuelbaraka572@gmail.com>
2025-12-04 12:10:58 +0300

    Modified .js file

--- script.js
+++ script.js
@@ -35,3 +35,37 @@ document.addEventListener("DOMContentLoaded", function() {
  });
   }
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+ 
+    function loadTasks() {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    storedTasks.forEach(taskText => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
+}
+
+
+function createTask(taskText, save = true) {
+     if (taskText === "") {
+        alert("Please enter a task");
+        return;
+     }
+     let list = document.createElement("li");
+        list.textContent = taskText;
+    
+     let btn = document.createElement("button");
+        btn.textContent = "Remove";
+        btn.classList.add = ("remove-btn");
+        btn.onclick = function () {
+          list.remove();
+   };
+
+        const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+        storedTasks.push(taskText);
+        localStorage.setItem('tasks', JSON.stringify(storedTasks));
+    }
+});
